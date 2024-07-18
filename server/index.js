4e15b67d86165821079d60dc80f2b639121d03fc
ishit <singhishit.06@gmail.com>
2024-07-18 17:31:36 +0530

    latest commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req,res)=>{
+    res.send("Server is working")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
