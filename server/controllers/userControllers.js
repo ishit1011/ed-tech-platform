faf5f066e19f610ad2e5a77d395ea3f4d7d29e5f
ishit <singhishit.06@gmail.com>
2024-07-22 01:52:03 +0530

    latest-commit

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,47 +2,67 @@ import { User } from "../models/userModel.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 import sendMail from "../middlewares/sendMail.js";
+import TryCatch from "../middlewares/TryCatch.js";
 
-export const register = async(req,res) =>{
-    try {
-        const {email, name, password} = req.body;
+export const register = TryCatch(async(req,res)=>{
+    const {email, name, password} = req.body;
 
-        let user = await User.findOne({email})
+    let user = await User.findOne({email})
 
-        if(user)
-            return res.status(400).json({
-                message: "User Already exists",
-            })
+    if(user)
+        return res.status(400).json({
+            message: "User Already exists",
+        })
 
-            const hashPassword = await bcrypt.hash(password,10);
+        const hashPassword = await bcrypt.hash(password,10);
             
-            user = {
-                name,
-                email,
-                password: hashPassword,
-            }
+        user = {
+            name,
+            email,
+            password: hashPassword,
+        }
 
-            const otp = Math.floor(Math.random() * 1000000);
+        const otp = Math.floor(Math.random() * 1000000);
 
-            const activationToken = jwt.sign({
-                user,otp,
-            },process.env.Activation_Secret,{
-                expiresIn: '5m'
-            })
+        const activationToken = jwt.sign({
+            user,otp,
+        },process.env.Activation_Secret,{
+            expiresIn: '5m'
+        })
 
-            const data = {
-                name,otp,
-            }
+        const data = {
+            name,otp,
+        }
 
-            await sendMail(email,"E-learning",data)
+        await sendMail(email,"E-learning",data)
 
-            res.status(200).json({
-                message: "Otp sent to your email", activationToken
-            })
+        res.status(200).json({
+            message: "Otp sent to your email", activationToken
+        })
+})
 
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
+export const verifyUser = TryCatch(async(req,res)=>{
+    const {otp, activationToken} = req.body;
+
+    const verify = jwt.verify(activationToken, process.env.Activation_Secret)
+
+    if(!verify){
+        return res.status(400).json({
+            message: "OTP Expired",
         })
     }
-}
+    if(verify.otp !== otp){
+        return res.status(400).json({
+            message: "Wrong OTP",
+        })
+    }
+    await User.create({
+        name: verify.user.name,
+        email: verify.user.email,
+        password: verify.user.password
+    })
+
+    res.json({
+        message: "User Registered"
+    })
+})
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { register } from '../controllers/userControllers.js';
+import { register, verifyUser } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.post("/user/register", register);
+router.post("/user/verify", verifyUser)
 
 export default router;
